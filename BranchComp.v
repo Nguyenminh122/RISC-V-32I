bdc189cb6bae6dde47a5ffdc99818daf6118862a
Nguyenminh122 <86726875+Nguyenminh122@users.noreply.github.com>
2024-11-17 20:23:06 +0700

    Update BranchComp.v

--- BranchComp.v
+++ BranchComp.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/04/2024 10:40:18 PM
-// Design Name: 
-// Module Name: BranchComp
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module BranchComp(BrEq, BrLT, A_in, B_in, BrUn);
     
